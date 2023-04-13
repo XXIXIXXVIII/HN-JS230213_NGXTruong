5db501494b66bab565091fd513624a132f9206cb
XXIXIXXVIII <xuantruong9x5@gmail.com>
2023-04-14 00:09:05 +0700

    day

--- src/Component/Page.js
+++ src/Component/Page.js
@@ -3,34 +3,62 @@ import { productsData } from "../data/productsData";
 
 function Page() {
   const [toSpend, setToSpend] = useState(128000000000);
+  const [toltal, setToltal] = useState(0);
+  const [menu, setMenu] = useState([]);
+
   const [countProduct, setCountProduct] = useState(() =>
     productsData.map((item) => {
       return { id: item.id, count: 0 };
     })
   );
 
-  const handleAdd = (item, index, item1) => {
+  const handleAdd = (item, index, itemData) => {
     const newCount = [...item];
     newCount[index].count++;
     setCountProduct(newCount);
-    setToSpend(toSpend-item1.price)
+
+    setToSpend(toSpend - itemData.price);
+    if (menu.length === 0) {
+      setMenu([...menu, itemData]);
+    } else {
+      if (menu.some((item) => item.id === itemData.id)) {
+        setMenu([...menu]);
+      } else {
+        setMenu([...menu, itemData]);
+      }
+    }
+    setToltal(toltal + itemData.price);
+    // console.log(menu)
   };
-  const handleInterest = (item, index,item1) => {
+  const handleInterest = (item, index, itemData) => {
     const newCount = [...item];
     newCount[index].count--;
     setCountProduct(newCount);
-    setToSpend(toSpend+item1.price)
+    setToSpend(toSpend + itemData.price);
+    setToltal(toltal - itemData.price);
   };
-  
+  const handleReset = () => {
+    setToSpend(128000000000);
+    setMenu([]);
+    setToltal(0);
+    setCountProduct(() =>
+      productsData.map((item) => {
+        return { id: item.id, count: 0 };
+      })
+    );
+  };
+
   return (
     <div className=" bg-gray-300">
       <div className="w-full h-14 font-light text-lg bg-red-500 fixed flex justify-center text-white [align-items:center] ">
         <span>
-          To Spend{" "}
-          <span className="font-bold text-xl">
+          To Spend
+          <span className="font-bold text-xl mx-1">
             {new Intl.NumberFormat().format(toSpend)}
-          </span>{" "}
-          You Have Money
+          </span>
+          {toSpend === 128000000000
+            ? "You Have Money"
+            : "You Have a Lot of money"}
         </span>
       </div>
       <div className="p-20 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
@@ -48,17 +76,21 @@ function Page() {
               </div>
               <div className="w-full px-4  flex py-5">
                 <button
-                  className="flex-grow h-10 rounded-l-lg border-2 border-red-500"
-                  onClick={() => handleInterest(countProduct,index,item)}
+                  className={`flex-grow h-10 rounded-l-lg border-2 border-red-500 ${
+                    countProduct.find((i) => i.id === item.id).count === 0
+                      ? "opacity-50 cursor-not-allowed"
+                      : "opacity-100 cursor-pointer"
+                  }`}
+                  onClick={() => handleInterest(countProduct, index, item)}
                 >
                   Interest
                 </button>
                 <span className="w-1/5 h-10 flex text-center flex-col justify-center">
-                  {countProduct.find((i)=>i.id===item.id).count}
+                  {countProduct.find((i) => i.id === item.id).count}
                 </span>
                 <button
                   className="w-1/5 h-10 rounded-r-lg bg-red-600"
-                  onClick={() => handleAdd(countProduct, index,item)}
+                  onClick={() => handleAdd(countProduct, index, item)}
                 >
                   Add
                 </button>
@@ -67,23 +99,41 @@ function Page() {
           );
         })}
       </div>
-      <div className="bg-white w-[90%] mx-auto">
-        <h3 className="text-center font-bold text-2xl my-5 text-red-600">
-          Your Cart
-        </h3>
-        <div className="flex justify-between w-[98%] mb-2 mx-auto px-4 bg-gray-300">
-          <span>Messi</span>
-          <span>5</span>
+
+      {toltal !== 0 ? (
+        <div className="bg-white w-[90%] mx-auto">
+          <h3 className="text-center font-bold text-2xl my-5 text-red-600">
+            Your Cart
+          </h3>
+
+          {menu.leng !== 0
+            ? menu.map((itemMenu, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between w-[98%] mb-2 mx-auto px-4 bg-gray-300"
+                >
+                  <span>{itemMenu.title}</span>
+                  <span>
+                    {countProduct.find((i) => i.id === itemMenu.id).count}
+                  </span>
+                </div>
+              ))
+            : "a"}
+          <div className="flex justify-between w-[98%] mx-auto  mt-5">
+            <span className="text-red-600 font-extrabold text-2xl">
+              Total: ${Intl.NumberFormat().format(toltal)}
+            </span>
+            <button
+              onClick={handleReset}
+              className="bg-red-600 text-white rounded-l-xl py-2 px-5"
+            >
+              reset Cart
+            </button>
+          </div>
         </div>
-        <div className="flex justify-between w-[98%] mx-auto  mt-5">
-          <span className="text-red-600 font-extrabold text-2xl">
-            Total: $123123
-          </span>
-          <button className="bg-red-600 text-white rounded-l-xl py-2 px-5">
-            reset Cart
-          </button>
-        </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
